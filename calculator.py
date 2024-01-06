28c8bc1a9c38a3a765e3e6dd006b359c61df3f99
Zach <zachgedge@gmail.com>
2024-01-06 05:30:06 -0500

    adding exception to display an error in the entry window and expanding entry window

--- calculator.py
+++ calculator.py
@@ -1,48 +1,50 @@
 
 from tkinter import *
 
-def button_click(value):
-    click = main_entry.get()
-    main_entry.delete(0, END)
-    main_entry.insert(0, click + value)
+def button_press(value):
+    current = entry_window.get()
+    entry_window.delete(0, END)
+    entry_window.insert(0, current + value)
 
 def calculate():
-    result = eval(main_entry.get())
-    main_entry.delete(0, END)
-    main_entry.insert(0, str(result))
-
+    try:
+        calc = entry_window.get()
+        entry_window.delete(0, END)
+        entry_window.insert(0, eval(str(calc)))
+    except Exception as e:
+        entry_window.delete(0, END)
+        entry_window.insert(0, f"ERROR: {str(e)}")
 
 
 root = Tk()
 root.title("Calculator")
-root.geometry("500x500")
+root.geometry('600x600')
 root.resizable(0,0)
 
 
-main_entry = Entry(root, width=20, font=('verdana', 18))
-main_entry.grid(row = 0, column=0, columnspan=4)
-
+entry_window = Entry(root, width=30, font=('verdana', 18))
+entry_window.grid(row = 0, column=0, columnspan=4)
 
 buttons = [
-
-    ("9", 1, 0), ("8", 1, 1), ("7", 1, 2), ('+', 1, 3),
-    ("6", 2, 0), ("5", 2, 1, ), ("4", 2, 2), ("-", 2,3),
-    ("3", 3, 0), ("2", 3, 1), ("1", 3, 2), ("*", 3, 3, ), 
-    ("0", 4, 0), ("%", 4, 1), ("/", 4, 2), (".", 4, 3)
+    ("9", 1,0), ("8", 1, 1), ("7", 1, 2), ("+", 1, 3), 
+    ("6", 2, 0), ("5", 2, 1), ("4", 2, 2), ("-", 2, 3), 
+    ("3", 3, 0), ("2", 3, 1), ("1", 3, 2), ("*", 3, 3), 
+    ("/", 4, 0), (".", 4, 1), ("%", 4, 2), ("$", 4, 3)  
 ]
+
 
 for (text, row, col) in buttons:
 
-    main_buttons = Button(root, text=text, padx=20, pady=20, command=lambda value = text: button_click(value) if value != "" else calculate())
+    main_buttons = Button(root, text=text, command = lambda value  = text:button_press(value) if value !="=" else calculate())
     main_buttons.grid(row = row, column=col, sticky=NSEW)
 
-equals_button = Button(root, text="=", font=('arial', 12), command=calculate)
-equals_button.grid(row = 5,column=0, columnspan=4)
+equels_button = Button(root, text="=", width=20 ,command=calculate)
+equels_button.grid(row = 5, column=0, columnspan=4, sticky=NSEW)
+
+for i in range(6):
+    root.columnconfigure(i, weight=2)
+    root.rowconfigure(i, weight=2)
 
 
-for i in range(4):
-    root.columnconfigure(i, weight=1)
-    root.rowconfigure(i, weight=1)
 
 root.mainloop()
-
