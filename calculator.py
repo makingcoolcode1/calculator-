b8f317c7aafddeb14747ed9048e8ca1847c6d478
makingcoolcode1 <zachgedge@gmail.com>
2025-04-30 20:47:54 -0400

    adding clear button

--- calculator.py
+++ calculator.py
@@ -1,35 +1,37 @@
 
 from tkinter import *
 
-def button_press(value):
-    insert_number = entry_window.get()
-    entry_window.delete(0, END)
-    entry_window.insert(0, insert_number + value )
+def insert_numbers(value):
+    number_text = entry_field.get()
+    entry_field.delete(0, END)
+    entry_field.insert(0, number_text + value)
 
 def calculate():
-    try:
-        calc_insert = entry_window.get()
-        result = eval(str(calc_insert))
-        entry_window.delete(0, END)
-        entry_window.insert(0, result)
-        print(result)
 
+    try:
+        execute = entry_field.get()
+        entry_field.delete(0, END)
+        entry_field.insert(0, eval(str(execute)))
+    
     except Exception as e:
-        entry_window.delete(0, END)
-        entry_window.insert(0, f"ERROR: {str(e)}")
+        entry_field.delete(0, END)
+        entry_field.insert(0, str(e))
 
 def clear_screen():
-    entry_window.delete(0, END)
+    entry_field.delete(0, END)
+
+
 
 root = Tk()
 root.title("Calculator")
 root.geometry("400x400")
 root.resizable(0,0)
 
-entry_window = Entry(root, width=40, font=("arial", 12))
-entry_window.grid(row=0, column=0, columnspan=4)
 
-buttons = [
+entry_field = Entry(root, width=30)
+entry_field.grid(row=0, column=0, columnspan=4)
+
+numbers = [
 
     ("9", 1, 0), ("8", 1, 1), ("7", 1, 2), ("+", 1, 3), 
     ("6", 2, 0), ("5", 2, 1), ("4", 2, 2), ("-", 2, 3), 
@@ -37,21 +39,20 @@ buttons = [
 
 ]
 
-for (text, row, col) in buttons:
-
-    main_buttons = Button(root, text=text, font=("arial", 10), command=lambda value = text:button_press(value) if value!="=" else calculate())
+for text, row, col in numbers:
+    main_buttons = Button(root, text=text, command=lambda value=text: insert_numbers(value) if entry_field != "=" else calculate)
     main_buttons.grid(row=row, column=col, sticky=NSEW)
 
-    equals_button = Button(root, text="=" , font=("arial", 12), command=calculate)
-    equals_button.grid(row=4, column=0, columnspan=4, sticky=NSEW)
+equals_btn = Button(root, text="=", command=calculate)
+equals_btn.grid(row=4, column=0, sticky=NSEW, columnspan=4)
 
-    clear_button = Button(root, text="C", command=clear_screen)
-    clear_button.grid(row=5, column=0, columnspan=4, sticky=NSEW)
+clear_btn = Button(root, text="C", width=5, command=clear_screen)
+clear_btn.grid(row=0, column=3)
 
-    for i in range(6):
-        root.columnconfigure(i, weight=2)
-        root.rowconfigure(i, weight=4)
-        
-        
+
+for i in range(4):
+    root.columnconfigure(i, weight=1)
+    root.rowconfigure(i, weight=1)
+
 
 root.mainloop()
